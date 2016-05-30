d942d9fa7008b96d904de76fd46a1a16e55c5230
qinmingyuan <mingyuan0715@foxmail.com>
2016-05-30 14:31:32 +0800

    update config

--- lib/the_role/config.rb
+++ lib/the_role/config.rb
@@ -5,13 +5,13 @@ module TheRole
                   :default_user_role,
                   :access_denied_method,
                   :login_required_method,
-                  :default_admin_email
+                  :default_admin_emails
 
   configure do |config|
     config.layout = :application
     config.default_user_role = nil
     config.access_denied_method = nil
     config.login_required_method  = nil
-    config.default_admin_email = nil
+    config.default_admin_emails = []
   end
 end
