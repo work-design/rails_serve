012d3f2112edb8348fd591464e59b95197341226
qinmingyuan <mingyuan0715@foxmail.com>
2016-06-13 19:20:06 +0800

    add cache for the role

--- app/models/concerns/the_role/role.rb
+++ app/models/concerns/the_role/role.rb
@@ -6,7 +6,20 @@ module TheRole
     included do
       has_many :role_rules, dependent: :destroy
       has_many :rules, through: :role_rules
-      serialize :the_role, JSON
+      has_many :sections, through: :rules
+    end
+
+    def the_role
+      Rails.cache.fetch("roles/#{self.id}") do
+        result = {}
+        sections.each do |section|
+        result[section.code] = {}
+          rules.where(section_id: section.id).each do |rule|
+            result[section.code].merge! rule.code => true
+          end
+        end
+        result
+      end
     end
 
     def create_section(section_name = nil)
