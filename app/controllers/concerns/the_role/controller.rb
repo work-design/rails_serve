2e3f62b2a0e9ba1f5f8526820928eaff48175eca
qinmingyuan <mingyuan0715@foxmail.com>
2016-06-18 14:44:10 +0800

    fix small style

--- app/controllers/concerns/the_role/controller.rb
+++ app/controllers/concerns/the_role/controller.rb
@@ -1,22 +1,6 @@
 module TheRole::Controller
 
-  def role_access_denied
-    access_denied_method = TheRole.config.access_denied_method
-    return send(access_denied_method) if access_denied_method && respond_to?(access_denied_method)
-
-    default_access_denied_response
-  end
-
-  private
-  def for_ownership_check obj
-    @owner_check_object = obj
-  end
-
-  def role_required(section = nil, rule = nil, &block)
-    if block_given?
-      yield block
-    end
-
+  def require_role
     if the_role_user.has_role? controller_path, action_name
       return true
     end
@@ -39,10 +23,27 @@ module TheRole::Controller
 
     role_access_denied
   end
-  alias :require_role :role_required
 
-  def owner_required
+  def require_owner
     role_access_denied unless the_role_user.owner? @owner_check_object
+  end
+
+  def check_role(section = nil, rule = nil, &block)
+    if block_given?
+      yield block
+    end
+  end
+
+  private
+  def role_access_denied
+    access_denied_method = TheRole.config.access_denied_method
+    return send(access_denied_method) if access_denied_method && respond_to?(access_denied_method)
+
+    default_access_denied_response
+  end
+
+  def for_ownership_check obj
+    @owner_check_object = obj
   end
 
   def default_access_denied_response
@@ -52,25 +53,20 @@ module TheRole::Controller
       render json: {
         errors: { the_role: [ access_denied_msg ] },
 
-        controller_name:      controller_path,
-        action_name:          action_name,
+        controller_name: controller_path,
+        action_name: action_name,
         has_access_to_action: the_role_user.try(:has_role?, controller_path, action_name),
-
-        the_role_user: { id: the_role_user.try(:id) },
-
-        owner_check_object: {
-          owner_check_object_id:    @owner_check_object.try(:id),
-          owner_check_object_class: @owner_check_object.try(:class).try(:to_s)
+        the_role_user: {
+          id: the_role_user.try(:id)
         },
-
+        owner_check_object: {
+          owner_check_object_id: @owner_check_object.try(:id),
+          owner_check_object_class: @owner_check_object.class.to_s
+        },
         has_access_to_object: the_role_user.try(:owner?, @owner_check_object)
       }, status: 401
     else
-      if request.referer.present?
-        redirect_to root_url, flash: { error: access_denied_msg }
-      else
-        redirect_to root_path, flash: { error: access_denied_msg }
-      end
+      redirect_back fallback_location: root_url, flash: { error: access_denied_msg }
     end
   end
 
