2c7db60e776c9e07cdaa6ccf7d84809705615bb1
qinmingyuan <mingyuan0715@foxmail.com>
2016-12-29 14:34:50 +0800

    update some pages

--- app/models/role_rule.rb
+++ app/models/role_rule.rb
@@ -1,15 +1,20 @@
 class RoleRule < ApplicationRecord
   belongs_to :role
   belongs_to :rule
-  belongs_to :section
+  belongs_to :section, optional: true
 
   after_commit :delete_cache, on: [:create, :destroy]
+  after_save :sync_section_id
 
   def delete_cache
     Rails.cache.delete("roles/#{role_id}")
     Who.pluck(:id).each do |who_id|
       Rails.cache.delete("who/#{who_id}")
     end
+  end
+
+  def sync_section_id
+    self.section_id = self.rule.section_id
   end
 
 end
