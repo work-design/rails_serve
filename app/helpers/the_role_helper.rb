ee2d6a1e0b0f3dab4bf1b61d96c39a45702ff69f
qinmingyuan <mingyuan0715@foxmail.com>
2017-07-11 10:23:01 +0800

    add method link to if permit

--- app/helpers/the_role_helper.rb
+++ app/helpers/the_role_helper.rb
@@ -1,8 +1,20 @@
 module TheRoleHelper
 
   def link_to_if_permit(name = nil, options = nil, html_options = nil, &block)
-    path = url_for
-    path_params = Rails.application.routes.recognize_path options, {method: options[:method]}
+    if block_given?
+      _options = name
+      _html_options = options
+    else
+      _options = options
+      _html_options = html_options
+    end
+
+    if _options.is_a? String
+      path_params = Rails.application.routes.recognize_path _options, { method: _html_options[:method] }
+    else
+      path_params = _options.slice(:controller, :action)
+    end
+
     if the_role_user.has_role? path_params[:controller], path_params[:action]
       link_to(name, options, html_options, &block)
     end
--- lib/the_role.rb
+++ lib/the_role.rb
@@ -3,7 +3,6 @@ require 'the_role/version'
 require 'the_role/engine'
 
 module TheRole
-  extend self
 
 
 end
