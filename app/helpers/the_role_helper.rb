7511174c2d803ff92944dd2d2c02ff340fee866e
qinmingyuan <mingyuan0715@foxmail.com>
2017-07-12 14:00:09 +0800

    the role

--- app/helpers/the_role_helper.rb
+++ app/helpers/the_role_helper.rb
@@ -2,10 +2,10 @@ module TheRoleHelper
 
   def link_to(name = nil, options = nil, html_options = nil, &block)
     if block_given?
-      _options = name
+      _options = name || {}
       _html_options = options
     else
-      _options = options
+      _options = options || {}
       _html_options = html_options
     end
 
@@ -13,6 +13,14 @@ module TheRoleHelper
       path_params = Rails.application.routes.recognize_path _options, { method: _html_options&.fetch(:method, nil) }
     else
       path_params = _options.slice(:controller, :action)
+    end
+    path_params[:controller] ||= controller_path
+    path_params[:action] ||= action_name
+
+    controller = RailsCom::Controllers.controller(path_params[:controller]).new
+    controller.action_name = path_params[:action]
+    unless controller.detect_filter(:require_role)
+      return super
     end
 
     if defined?(the_role_user) && the_role_user.has_role?(path_params[:controller], path_params[:action])
