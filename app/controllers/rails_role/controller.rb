a75773cd77239e81c4bc9e1e2adba42f7f73b756
qinmingyuan <mingyuan0715@foxmail.com>
2019-05-16 09:45:42 +0800

    simple method

--- app/controllers/rails_role/controller.rb
+++ app/controllers/rails_role/controller.rb
@@ -4,36 +4,29 @@ module RailsRole::Controller
     helper_method :rails_role_user
   end
 
-  def require_role(the_params = params['id'])
-    if rails_role_user.has_role? controller_path, action_name, the_params
+  def require_role(role_params = params['id'])
+    if rails_role_user.has_role? controller_path, action_name, role_params
       return true
     end
 
     role_access_denied
   end
 
-  private
-  def role_access_denied
-    access_denied_method = RailsRole.config.access_denied_method
-    return send(access_denied_method) if access_denied_method && respond_to?(access_denied_method)
-
-    default_access_denied_response
+  def rails_role_user
+    defined?(current_user) && current_user
   end
 
-  def default_access_denied_response
-    access_denied_msg = I18n.t(:access_denied, scope: :rails_role)
+  private
+  def role_access_denied
+    message = I18n.t(:access_denied, scope: :rails_role)
 
     if request.xhr?
       render file: RailsRole::Engine.root + 'app/views/role/admin/base/errors.js.erb', status: 403
     elsif request.format.json?
       raise ActionController::ForbiddenError
     else
-      redirect_back fallback_location: root_url, error: access_denied_msg
+      redirect_back fallback_location: root_url, error: message
     end
   end
-
-  def rails_role_user
-    defined?(current_user) && current_user
-  end
-
+  
 end
--- lib/rails_role/config.rb
+++ lib/rails_role/config.rb
@@ -1,9 +1,8 @@
 module RailsRole
   include ActiveSupport::Configurable
-  config_accessor :access_denied_method, :default_admin_emails
+  config_accessor :default_admin_emails
 
   configure do |config|
-    config.access_denied_method = nil
     config.default_admin_emails = []
     config.admin_controller = 'AdminController'
     config.ignore_controllers = []
