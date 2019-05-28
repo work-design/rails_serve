cefe25f17cec6a3d4741fa54de34dcf8da3ae258
qinmingyuan <mingyuan0715@foxmail.com>
2019-05-28 18:53:41 +0800

    rails role helper

--- app/helpers/rails_role/helper.rb
+++ app/helpers/rails_role/helper.rb
@@ -1,36 +1,47 @@
 module RailsRole::Helper
+  
+  def link_to_if(name = nil, options = nil, html_options = nil, &block)
+    condition = role_permit?(options, html_options)
+    binding.pry
+    super(condition, name, options, html_options, &block)
+  end
 
   def link_to(name = nil, options = nil, html_options = nil, &block)
     if block_given?
-      _options = name || {}
+      _options = name
       _html_options = options
     else
-      _options = options || {}
+      _options = options
       _html_options = html_options
     end
-
+    
+    if role_permit?(_options, _html_options)
+      super
+    end
+  end
+  
+  def role_permit?(_options, _html_options)
     if _options.is_a? String
       begin
         path_params = Rails.application.routes.recognize_path _options, { method: _html_options&.fetch(:method, nil) }
       rescue ActionController::RoutingError
-        return super
+        return true
       end
-    elsif _options.is_a? Symbol
-      return super
+    elsif _options == :back
+      return true
     else
       path_params = _options.slice(:controller, :action)
     end
     path_params[:controller] ||= controller_path
     path_params[:action] ||= action_name
-
-    controller = RailsCom::Controllers.controller(path_params[:controller]).new
-    controller.action_name = path_params[:action]
+    
+    controller = RailsCom::Controllers.controller(path_params[:controller], path_params[:action])
     unless controller.detect_filter(:require_role)
-      return super
+      return true
     end
 
     if defined?(rails_role_user) && rails_role_user&.has_role?(path_params[:controller], path_params[:action])
-      super
+      true
     end
   end
 
