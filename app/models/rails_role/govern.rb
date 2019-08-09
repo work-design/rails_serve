0284c8896a9306dd74c71849e76630d81d1bc4ec
qinmingyuan <mingyuan0715@foxmail.com>
2019-08-09 23:46:32 +0800

    compute roles

--- app/models/rails_role/govern.rb
+++ app/models/rails_role/govern.rb
@@ -6,6 +6,7 @@ module RailsRole::Govern
     belongs_to :govern_taxon, counter_cache: true, optional: true
     has_many :rules, -> { order(position: :asc) }, dependent: :destroy
     has_many :role_rules, dependent: :destroy
+    accepts_nested_attributes_for :rules, allow_destroy: true
     
     default_scope -> { order(position: :asc, id: :asc) }
     scope :without_taxon, -> { where(govern_taxon_id: nil) }
--- app/models/rails_role/govern/controller_govern.rb
+++ app/models/rails_role/govern/controller_govern.rb
@@ -1,33 +1,41 @@
 module RailsRole::Govern::ControllerGovern
   extend ActiveSupport::Concern
-
-  def sync_rules
-    all_actions = ['admin', 'read'] + RailsCom::Routes.actions(self.code)
-    govern_rules = self.rules.pluck(:code)
-
-    (all_actions - govern_rules).each do |la|
-      self.rules.create(code: la)
-    end
-
-    invalid_rules = (govern_rules - all_actions)
-    invalid_rules.each do |la|
-      self.rules.find_by(code: la).destroy
+  included do
+    after_create_commit :sync_govern_taxon
+  end
+  
+  def sync_govern_taxon
+    path = code.split('/')
+    path.pop
+    if path.size > 0
+      taxon = GovernTaxon.find_by code: path.join('/')
+      self.govern_taxon_id = taxon&.id
+      self.save
     end
   end
 
   class_methods do
     def sync_controllers
       missing_controllers, invalid_controllers = analyze_controllers
-      missing_controllers.each do |controller|
-        govern = ControllerGovern.new code: controller
-        path = controller.split('/')
-        path.pop
-        if path.size > 0
-          taxon = GovernTaxon.find_by code: path.join('/')
-          govern.govern_taxon_id = taxon&.id
+      RailsCom::Routes.controllers.extract!(*missing_controllers).each do |controller, routes|
+        govern = ControllerGovern.find_or_initialize_by(code: controller)
+        present_rules = govern.rules.pluck(:code)
+        
+        all_rules = routes.select! do |route|
+          controller = RailsCom::Controllers.controller(controller, route[:action])
+          controller.detect_filter(:require_role)
+        end.map(&->(i){ i[:action] })
+        all_rules += ['admin', 'read']
+        
+        (all_rules - present_rules).each do |action|
+          self.rules.build(code: action)
+        end
+
+        (present_rules - all_rules).each do |action|
+          r = self.rules.find_by(code: action)
+          r.mark_for_destruction
         end
         govern.save
-        govern.sync_rules
       end
       ControllerGovern.where(code: invalid_controllers).each do |controller|
         controller.destroy
@@ -36,7 +44,7 @@ module RailsRole::Govern::ControllerGovern
 
     def analyze_controllers
       present_controllers = ControllerGovern.unscoped.select(:code).distinct.pluck(:code)
-      all_controllers = RailsCom::Routes.controllers - RailsRole.config.ignore_controllers
+      all_controllers = RailsCom::Routes.controllers.except!(*RailsRole.config.ignore_controllers).keys
 
       missing_controllers = all_controllers - present_controllers
       invalid_controllers = present_controllers - all_controllers
