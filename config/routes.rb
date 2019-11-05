8551c79f0fad7ce319fc7eabff925dcc4b0a74a4
qinmingyuan <mingyuan0715@foxmail.com>
2019-11-05 22:33:21 +0800

    just index and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ Rails.application.routes.draw do
   
   scope module: 'role' do
     scope path: ':who_type/:who_id' do
-      resource :who_roles
+      resource :who_roles, only: [:show, :edit, :update]
     end
   end
   
