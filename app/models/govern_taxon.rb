859a0a1cc8d34c1c4c0ca249a49db8a517dbfcef
qinmingyuan <mingyuan0715@foxmail.com>
2020-03-18 02:16:38 +0800

    govern taxon with tree

--- app/models/govern_taxon.rb
+++ app/models/govern_taxon.rb
@@ -1,3 +1,4 @@
 class GovernTaxon < ApplicationRecord
   include RailsRole::GovernTaxon
+  prepend RailsTaxon::Node
 end unless defined? GovernTaxon
--- app/models/rails_role/govern_taxon.rb
+++ app/models/rails_role/govern_taxon.rb
@@ -5,11 +5,17 @@ module RailsRole::GovernTaxon
     attribute :name, :string
     attribute :position, :integer
     attribute :governs_count, :integer, default: 0
-    
+
     has_many :governs, -> { order(position: :asc) }, dependent: :nullify
     has_many :rules, through: :governs
     default_scope -> { order(position: :asc, id: :asc) }
     validates :code, uniqueness: true
+
+    before_save :name_from_code, if: -> { name.blank? }
+  end
+
+  def name_from_code
+    self.name ||= I18n.t "#{code.split('/').join('.')}.title", default: nil
   end
 
   def desc
@@ -17,16 +23,22 @@ module RailsRole::GovernTaxon
   end
 
   class_methods do
+
     def sync_modules
       missing_modules.each do |m|
-        GovernTaxon.create code: m
+        gt = GovernTaxon.find_or_initialize_by(code: m.join('/'))
+        gt.parent = GovernTaxon.find_by(code: m[0..-2].join('/')) if m.size > 1
+        gt.save
       end
     end
 
     def missing_modules
-      RailsCom::Routes.modules - GovernTaxon.unscoped.select(:code).distinct.pluck(:code)
+      existing = GovernTaxon.unscoped.select(:code).distinct.pluck(:code).map(&->(i){ i.split('/') })
+      r = (RailsCom::Routes.modules.keys - existing).sort_by(&:size)
+      r.delete([])
+      r
     end
-  end
 
+  end
 
 end
