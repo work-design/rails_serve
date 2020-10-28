2153a3ce7be9c9669befa7c05b4e56d026f18a63
qinmingyuan <mingyuan0715@foxmail.com>
2020-10-28 16:08:09 +0800

    govern must have an taxon

--- app/models/rails_role/govern.rb
+++ app/models/rails_role/govern.rb
@@ -2,21 +2,21 @@ module RailsRole::Govern
   extend ActiveSupport::Concern
 
   included do
-    acts_as_list
     attribute :type, :string
     attribute :name, :string
     attribute :code, :string
     attribute :position, :integer
 
-    belongs_to :govern_taxon, counter_cache: true, optional: true
+    belongs_to :govern_taxon, counter_cache: true
     has_many :rules, -> { order(position: :asc) }, dependent: :destroy
     has_many :role_rules, dependent: :destroy
     accepts_nested_attributes_for :rules, allow_destroy: true
 
     default_scope -> { order(position: :asc, id: :asc) }
-    scope :without_taxon, -> { where(govern_taxon_id: nil) }
 
     validates :code, uniqueness: true
+
+    acts_as_list
   end
 
   def desc
