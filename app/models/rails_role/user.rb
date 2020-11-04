ca8e3273a13af21081ef30d68efcb5f9695c7517
qinmingyuan <mingyuan0715@foxmail.com>
2020-11-04 12:00:36 +0800

    unify to user

--- app/models/rails_role/user.rb
+++ app/models/rails_role/user.rb
@@ -1,6 +1,5 @@
 module RailsRole::User
   extend ActiveSupport::Concern
-  include RailsRoleExt::Base
 
   included do
     attribute :cached_role_ids, :integer, array: true
@@ -28,6 +27,35 @@ module RailsRole::User
     end
 
     result
+  end
+
+  def has_role?(business: 'application', namespace: 'application', controller:, action:, params: {})
+    if respond_to?(:admin?) && admin?
+      return true
+    end
+
+    options = [business.to_s, namespace.to_s, controller.to_s, action.to_s].take_while(&:present?)
+    return false if options.blank?
+    role_hash.dig(*options).present?
+  end
+
+  def any_role?(*any_roles, **roles_hash)
+    if respond_to?(:admin?) && admin?
+      return true
+    end
+
+    if (any_roles.map(&:to_s) & rails_role.keys).present?
+      return true
+    end
+
+    roles_hash.stringify_keys!
+    roles_hash.slice(*rails_role.keys).each do |govern, rules|
+      h_keys = rails_role[govern].select { |i| i }.keys
+      rules = Array(rules).map(&:to_s)
+      return true if (h_keys & rules).present?
+    end
+
+    false
   end
 
   def sync_to_role_ids
