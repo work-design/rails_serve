19654e0bb13775dde8c2128be24b61435f2c8e0a
qinmingyuan <mingyuan0715@foxmail.com>
2021-01-04 16:46:12 +0800

    role

--- app/models/rails_role/role_rule.rb
+++ app/models/rails_role/role_rule.rb
@@ -4,7 +4,8 @@ module RailsRole::RoleRule
   included do
     attribute :business_identifier, :string
     attribute :namespace_identifier, :string
-    attribute :controller_identifier, :string
+    attribute :controller_path, :string
+    attribute :controller_name, :string
     attribute :action_name, :string
     attribute :params_name, :string
     attribute :params_identifier, :string
@@ -12,8 +13,8 @@ module RailsRole::RoleRule
     belongs_to :role
     belongs_to :busyness, foreign_key: :business_identifier, primary_key: :identifier, optional: true
     belongs_to :name_space, foreign_key: :namespace_identifier, primary_key: :identifier, optional: true
-    belongs_to :govern, foreign_key: :controller_identifier, primary_key: :identifier, optional: true
-    belongs_to :rule, ->(o){ where(controller_identifier: o.controller_identifier) }, foreign_key: :action_name, primary_key: :action_name, optional: true
+    belongs_to :govern, foreign_key: :controller_name, primary_key: :controller_name, optional: true
+    belongs_to :rule, ->(o){ where(business_identifier: o.business_identifier, namespace_identifier: o.namespace_identifier, controller_name: o.controller_name) }, foreign_key: :action_name, primary_key: :action_name, optional: true
   end
 
 end
