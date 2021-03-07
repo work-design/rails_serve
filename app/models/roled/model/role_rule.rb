c048cde7c707683ac0fafb0a8abe5775860475b9
qinmingyuan <mingyuan0715@foxmail.com>
2021-03-07 21:07:54 +0800

    role rule business default as

--- app/models/roled/model/role_rule.rb
+++ app/models/roled/model/role_rule.rb
@@ -3,8 +3,8 @@ module Roled
     extend ActiveSupport::Concern
 
     included do
-      attribute :business_identifier, :string
-      attribute :namespace_identifier, :string
+      attribute :business_identifier, :string, default: ''
+      attribute :namespace_identifier, :string, default: ''
       attribute :controller_path, :string
       attribute :action_name, :string
       attribute :params_name, :string
