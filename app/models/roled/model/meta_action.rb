79d867a7528fbe40bae136d5dfff89a1eb233834
qinmingyuan <mingyuan0715@foxmail.com>
2022-02-27 00:03:30 +0800

    fix has many role rules

--- app/models/roled/model/meta_action.rb
+++ app/models/roled/model/meta_action.rb
@@ -3,8 +3,14 @@ module Roled
     extend ActiveSupport::Concern
 
     included do
-      has_many :role_rules, ->(o) { where(controller_path: o.controller_path) }, foreign_key: :action_name, primary_key: :identifier, dependent: :delete_all
+      has_many :role_rules, ->(o) { where(controller_path: o.controller_path) }, foreign_key: :action_name, primary_key: :action_name, dependent: :delete_all
       has_many :roles, through: :role_rules
+
+      after_destroy_commit :prune_from_role
+    end
+
+    def prune_from_role
+
     end
 
   end
