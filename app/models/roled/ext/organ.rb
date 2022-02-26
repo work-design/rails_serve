8af5077b407e9535fb1e658ebfa30378dc635bcb
qinmingyuan <mingyuan0715@foxmail.com>
2022-02-26 14:52:23 +0800

    roleds

--- app/models/roled/ext/organ.rb
+++ app/models/roled/ext/organ.rb
@@ -3,7 +3,8 @@ module Roled
     extend ActiveSupport::Concern
 
     included do
-      has_many :who_roles, class_name: 'Roled::WhoOrganRole', foreign_key: :who_id, dependent: :destroy_async
+      has_many :who_roles, class_name: 'Roled::WhoOrganRole', foreign_key: :who_id, inverse_of: :who, dependent: :destroy_async
+      accepts_nested_attributes_for :who_roles
       include Ext::Base
     end
 
