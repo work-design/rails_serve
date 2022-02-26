6adced5b7a2a228e27b24d19dca259535627ccdb
qinmingyuan <mingyuan0715@foxmail.com>
2022-02-27 00:12:41 +0800

    prune when meta action destroyed

--- app/models/roled/model/meta_action.rb
+++ app/models/roled/model/meta_action.rb
@@ -6,11 +6,13 @@ module Roled
       has_many :role_rules, ->(o) { where(controller_path: o.controller_path) }, foreign_key: :action_name, primary_key: :action_name, dependent: :delete_all
       has_many :roles, through: :role_rules
 
-      after_destroy_commit :prune_from_role
+      #after_destroy_commit :prune_from_role
     end
 
     def prune_from_role
-
+      roles.each do |role|
+        role.action_off(self)
+      end
     end
 
   end
