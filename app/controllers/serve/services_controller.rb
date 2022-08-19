a32fa80bd8c13b24160caf74b17799d9c3562874
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-20 00:33:23 +0800

    list services

--- app/controllers/serve/services_controller.rb
+++ app/controllers/serve/services_controller.rb
@@ -1,8 +1,13 @@
 module Serve
   class ServicesController < BaseController
+    before_action :set_cart
 
     def index
       @services = Service.default_where(default_params)
+    end
+
+    def set_cart
+      @cart = current_carts.find_or_create_by(good_type: 'Serve::Service', aim: 'use')
     end
 
   end
