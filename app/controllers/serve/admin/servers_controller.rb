376235c6aa3e3c81b52f5d7bc0660034f41d9540
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-19 23:28:35 +0800

    server params

--- app/controllers/serve/admin/servers_controller.rb
+++ app/controllers/serve/admin/servers_controller.rb
@@ -1,10 +1,23 @@
 module Serve
   class Admin::ServersController < Admin::BaseController
     before_action :set_service
+    before_action :set_new_server, only: [:new, :create]
 
     private
     def set_service
       @service = Service.find params[:service_id]
     end
+
+    def set_new_server
+      @server = @service.servers.build(server_params)
+    end
+
+    def server_params
+      params.fetch(:server, {}).permit(
+        :name,
+        :member_id
+      )
+    end
+
   end
 end
