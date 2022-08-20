e22203f4cdd50d1aa1d4b261987f68ec5daebc86
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-20 17:03:30 +0800

    start and finish works

--- app/controllers/serve/me/servings_controller.rb
+++ app/controllers/serve/me/servings_controller.rb
@@ -1,13 +1,19 @@
 module Serve
   class Me::ServingsController < Me::BaseController
-    before_action :set_serving, only: [:qrcode, :claim]
+    before_action :set_serving, only: [:qrcode, :start, :finish]
 
     def qrcode
 
     end
 
-    def claim
+    def start
       @serving.member = current_member
+      @serving.start_at = Time.current
+      @serving.save
+    end
+
+    def finish
+      @serving.finish_at = Time.current
       @serving.save
     end
 
