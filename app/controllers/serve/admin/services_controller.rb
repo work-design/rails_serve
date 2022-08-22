d56b2ebc4d506141ba1295433454fd9463dd1010
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-22 23:18:07 +0800

    card

--- app/controllers/serve/admin/services_controller.rb
+++ app/controllers/serve/admin/services_controller.rb
@@ -1,6 +1,9 @@
 module Serve
   class Admin::ServicesController < Admin::BaseController
-    before_action :set_service, only: [:show, :edit, :wallet, :update_wallet, :update, :destroy, :actions]
+    before_action :set_service, only: [
+      :show, :edit, :update, :destroy, :actions,
+      :wallet, :update_wallet, :card, :update_card
+    ]
 
     def wallet
       @wallet_templates = Trade::WalletTemplate.default_where(default_params)
@@ -8,6 +11,15 @@ module Serve
 
     def update_wallet
       @service.wallet_price = wallet_price_params
+      @service.save
+    end
+
+    def card
+      @card_templates = Trade::CardTemplate.default_where(default_params)
+    end
+
+    def update_card
+      @service.card_price = card_price_params
       @service.save
     end
 
@@ -20,6 +32,16 @@ module Serve
       r = {}
 
       params.dig(:service, :wallet_price).each do |_, v|
+        r.merge! v[:code] => v[:price]
+      end
+
+      r
+    end
+
+    def card_price_params
+      r = {}
+
+      params.dig(:service, :card_price).each do |_, v|
         r.merge! v[:code] => v[:price]
       end
 
