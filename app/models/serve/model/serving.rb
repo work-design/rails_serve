21a7551accd43e01382309a1720c8015ca511a0f
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-28 00:29:44 +0800

    auto reload

--- app/models/serve/model/serving.rb
+++ app/models/serve/model/serving.rb
@@ -12,6 +12,8 @@ module Serve
       belongs_to :member, class_name: 'Org::Member', optional: true
       belongs_to :wallet_payment, class_name: 'Trade::WalletPayment', optional: true
       belongs_to :item, class_name: 'Trade::Item'
+
+      after_save_commit :send_notice, if: -> { (saved_changes.keys & ['member_id', 'start_at', 'finish_at']).present? }
     end
 
     def sync_from
@@ -28,6 +30,16 @@ module Serve
 
     def qrcode_enter_url
       QrcodeHelper.data_url(enter_url)
+    end
+
+    def send_notice
+      broadcast_action_to(
+        self,
+        action: :update,
+        target: "serving_#{id}",
+        partial: 'serve/my/servings/_index/serving_preview',
+        locals: { model: self }
+      )
     end
 
   end
